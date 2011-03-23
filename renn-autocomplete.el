58c661e229136e4f4f7f7744642013e3d35203c5
seorenn <hirenn@gmail.com>
2011-03-23 15:29:14 +0900

    added auto-complete

---
+++ renn-autocomplete.el
@@ -0,0 +1,4 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete/ac-dict")
+(ac-config-default)
