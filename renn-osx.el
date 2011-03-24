458b6e1710a217a762ba7df9e78f058df2f332c3
Seorenn <hirenn@gmail.com>
2011-03-24 22:26:14 +0900

    changed OS X Japanese Font

--- renn-osx.el
+++ renn-osx.el
@@ -7,3 +7,5 @@
 
 (set-face-font 'default "Monaco-11")
 (set-fontset-font "fontset-default" '(#x1100 . #xffdc) "AppleGothic-14")
+(set-fontset-font "fontset-default" 'kana "Hiragino Kaku Gothic Pro-14")
+(set-fontset-font "fontset-default" 'han "Hiragino Kaku Gothic Pro-14")
