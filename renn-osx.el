db26614383df2b4d8c6e1133c4deb94cb6ea4b39
Seorenn <hirenn@gmail.com>
2011-03-27 22:20:38 +0900

    turn off using native-IME

--- renn-osx.el
+++ renn-osx.el
@@ -7,5 +7,3 @@
 (set-fontset-font "fontset-default" '(#x1100 . #xffdc) "AppleGothic-14")
 (set-fontset-font "fontset-default" 'kana "Hiragino Kaku Gothic Pro-14")
 (set-fontset-font "fontset-default" 'han "Hiragino Kaku Gothic Pro-14")
-
-(setq default-input-method "MacOSX")
