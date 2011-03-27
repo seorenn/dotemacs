7a0570d12ef9ef07c99b7d8eec842dddb5bf94f7
Seorenn <hirenn@gmail.com>
2011-03-27 15:53:09 +0900

    disable pymacs(some buggy...)

--- renn-osx.el
+++ renn-osx.el
@@ -1,11 +1,10 @@
-(setq mac-option-key-is-meta nil)
-(setq mac-command-key-is-meta t)
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier nil)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
 
-(aquamacs-autoface-mode nil)
-
-(Set-face-font 'default "Monaco-11")
+(set-face-font 'default "Monaco-11")
 (set-fontset-font "fontset-default" '(#x1100 . #xffdc) "AppleGothic-14")
 (set-fontset-font "fontset-default" 'kana "Hiragino Kaku Gothic Pro-14")
 (set-fontset-font "fontset-default" 'han "Hiragino Kaku Gothic Pro-14")
+
+(setq default-input-method "MacOSX")
