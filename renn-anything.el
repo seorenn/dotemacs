6a41e6188e02ef0a530c6ef32efc0175e0695684
seorenn <hirenn@gmail.com>
2011-04-01 10:30:49 +0900

    disable ido-mode, anything will do it

--- renn-anything.el
+++ renn-anything.el
@@ -1,4 +1,7 @@
 (require 'anything-startup)
 
+(global-set-key "\C-x\C-f" 'anything-for-files)
+(global-set-key [(ctrl tab)] 'anything-for-buffers)
+
 ; bypass troubles with ido
-(anything-read-string-mode nil)
+;(anything-read-string-mode nil)
