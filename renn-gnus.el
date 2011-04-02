9dd3047414eff35f64eb33e75810c59cfdb288a9
Seorenn <hirenn@gmail.com>
2011-04-02 23:45:33 +0900

    added some link...

--- renn-gnus.el
+++ renn-gnus.el
@@ -1,3 +1,6 @@
+;; Refer this site: for GNUS more speed-up!
+;; http://www.emacswiki.org/emacs/GnusSpeed
+
 (gnus-compile)
 (setq gc-cons-threshold 3500000)
 (setq gnus-use-correct-string-widths nil)
