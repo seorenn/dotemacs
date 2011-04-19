17d2d10979ccbe81c32f53a941d96a9020cfd778
seorenn <hirenn@gmail.com>
2011-04-19 18:38:23 +0900

    some code under development

--- renn-func.el
+++ renn-func.el
@@ -40,3 +40,87 @@
     (setq target-url (concat "http://translate.google.com/#auto|ko|" sel-word))
 
     (browse-url target-url)))
+
+(defun files-in-below-directory (directory)
+  "List the .el files in DIRECTORY and in its sub-directories."
+  ;; Although the function will be used non-interactively,
+  ;; it will be easier to test if we make it interactive.
+  ;; The directory will have a name such as
+  ;;  "/usr/local/share/emacs/21.0.100/lisp/"
+  (interactive "DDirectory name: ")
+  (let (el-files-list
+        (current-directory-list
+         (directory-files-and-attributes directory t)))
+    ;; while we are in the current directory
+    (while current-directory-list
+      (cond
+       ;; check to see whether filename ends in `.el'
+       ;; and if so, append its name to a list.
+       ((equal ".el" (substring (car (car current-directory-list)) -3))
+        (setq el-files-list
+              (cons (car (car current-directory-list)) el-files-list)))
+       ;; check whether filename is that of a directory
+       ((eq t (car (cdr (car current-directory-list))))
+        ;; decide whether to skip or recurse
+        (if
+            (equal "."
+                   (substring (car (car current-directory-list)) -1))
+            ;; then do nothing since filename is that of
+            ;;   current directory or parent, "." or ".."
+            ()
+          ;; else descend into the directory and repeat the process
+          (setq el-files-list
+                (append
+                 (files-in-below-directory
+                  (car (car current-directory-list)))
+                 el-files-list)))))
+      ;; move to the next filename in the list; this also
+      ;; shortens the list so the while loop eventually comes to an end
+      (setq current-directory-list (cdr current-directory-list)))
+    ;; return the filenames
+    el-files-list))
+
+(defun string-match-list (src-string pattern-list)
+  "Evaluate src-string matches pattern-list's each pattern."
+  (let (result)
+    (setq result nil)
+    (if pattern-list (mapcar
+                      (lambda (tmpdir) (setq result (string-match tmpdir src-string)))
+                      pattern-list)
+      (setq result t))
+    result))
+
+(defun files-under-dir-with-except (directory except-dirs except-exts)
+  "List the files in DIRECTORY and in its sub-directories except some directories and extensions"
+  (interactive)
+  (let (el-files-list
+        (current-directory-list
+         (directory-files-and-attributes directory t)))
+    ;; while we are in the current directory
+    (while current-directory-list
+      (cond
+       ;; check to see whether filename ends in `.el'
+       ;; and if so, append its name to a list.
+       ((equal ".el" (substring (car (car current-directory-list)) -3))
+        (setq el-files-list
+              (cons (car (car current-directory-list)) el-files-list)))
+       ;; check whether filename is that of a directory
+       ((eq t (car (cdr (car current-directory-list))))
+        ;; decide whether to skip or recurse
+        (if
+            (equal "."
+                   (substring (car (car current-directory-list)) -1))
+            ;; then do nothing since filename is that of
+            ;;   current directory or parent, "." or ".."
+            ()
+          ;; else descend into the directory and repeat the process
+          (setq el-files-list
+                (append
+                 (files-in-below-directory
+                  (car (car current-directory-list)))
+                 el-files-list)))))
+      ;; move to the next filename in the list; this also
+      ;; shortens the list so the while loop eventually comes to an end
+      (setq current-directory-list (cdr current-directory-list)))
+    ;; return the filenames
+    el-files-list))
