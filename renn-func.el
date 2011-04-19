2c9be46b8d775aaf4100f9013c83025eb7b65e87
seorenn <hirenn@gmail.com>
2011-04-19 18:45:41 +0900

    some code under development

--- renn-func.el
+++ renn-func.el
@@ -90,10 +90,11 @@
       (setq result t))
     result))
 
-(defun files-under-dir-with-except (directory except-dirs except-exts)
+; TODO - check exception list
+(defun files-under-dir-with-except (directory except-list)
   "List the files in DIRECTORY and in its sub-directories except some directories and extensions"
   (interactive)
-  (let (el-files-list
+  (let (tmp-files-list
         (current-directory-list
          (directory-files-and-attributes directory t)))
     ;; while we are in the current directory
@@ -102,8 +103,8 @@
        ;; check to see whether filename ends in `.el'
        ;; and if so, append its name to a list.
        ((equal ".el" (substring (car (car current-directory-list)) -3))
-        (setq el-files-list
-              (cons (car (car current-directory-list)) el-files-list)))
+        (setq tmp-files-list
+              (cons (car (car current-directory-list)) tmp-files-list)))
        ;; check whether filename is that of a directory
        ((eq t (car (cdr (car current-directory-list))))
         ;; decide whether to skip or recurse
@@ -114,13 +115,13 @@
             ;;   current directory or parent, "." or ".."
             ()
           ;; else descend into the directory and repeat the process
-          (setq el-files-list
+          (setq tmp-files-list
                 (append
                  (files-in-below-directory
                   (car (car current-directory-list)))
-                 el-files-list)))))
+                 tmp-files-list)))))
       ;; move to the next filename in the list; this also
       ;; shortens the list so the while loop eventually comes to an end
       (setq current-directory-list (cdr current-directory-list)))
     ;; return the filenames
-    el-files-list))
+    tmp-files-list))
