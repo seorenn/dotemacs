c675e0f2f6ad312bcf9249f80ffd58ab7c5bfdd1
Seorenn <hirenn@gmail.com>
2011-04-20 21:44:12 +0900

    commented unused codes

--- renn-func.el
+++ renn-func.el
@@ -67,45 +67,6 @@
         (add-hook 'isearch-mode-hook 'isearch-set-initial-string)
         (isearch-forward regexp-p no-recursive-edit)))))
 
-(defun files-in-below-directory (directory)
-  "List the .el files in DIRECTORY and in its sub-directories."
-  ;; Although the function will be used non-interactively,
-  ;; it will be easier to test if we make it interactive.
-  ;; The directory will have a name such as
-  ;;  "/usr/local/share/emacs/21.0.100/lisp/"
-  (interactive "DDirectory name: ")
-  (let (el-files-list
-        (current-directory-list
-         (directory-files-and-attributes directory t)))
-    ;; while we are in the current directory
-    (while current-directory-list
-      (cond
-       ;; check to see whether filename ends in `.el'
-       ;; and if so, append its name to a list.
-       ((equal ".el" (substring (car (car current-directory-list)) -3))
-        (setq el-files-list
-              (cons (car (car current-directory-list)) el-files-list)))
-       ;; check whether filename is that of a directory
-       ((eq t (car (cdr (car current-directory-list))))
-        ;; decide whether to skip or recurse
-        (if
-            (equal "."
-                   (substring (car (car current-directory-list)) -1))
-            ;; then do nothing since filename is that of
-            ;;   current directory or parent, "." or ".."
-            ()
-          ;; else descend into the directory and repeat the process
-          (setq el-files-list
-                (append
-                 (files-in-below-directory
-                  (car (car current-directory-list)))
-                 el-files-list)))))
-      ;; move to the next filename in the list; this also
-      ;; shortens the list so the while loop eventually comes to an end
-      (setq current-directory-list (cdr current-directory-list)))
-    ;; return the filenames
-    el-files-list))
-
 (defun string-not-match-list (src-string pattern-list)
   "Evaluate src-string not matches pattern-list's each pattern."
   (let (result (tmp-pattern-list pattern-list))
@@ -117,8 +78,14 @@
       (setq tmp-pattern-list (cdr tmp-pattern-list)))
     result))
 
-;; TODO: add to list when file, not directory.
-;;       recursive navigation not works well...
+(defun is-file-attr (file-attr)
+  (let (result (tmp-attr file-attr))
+    (setq result nil)
+    (when (stringp (car tmp-attr))
+      (setq tmp-attr (file-attributes (car tmp-attr))))
+    (unless (car tmp-attr) (setq result t))
+    result))
+
 (defun files-under-dir-with-except (directory except-list)
   "List the files in DIRECTORY and in its sub-directories except some directories and extensions"
   (interactive)
@@ -127,8 +94,7 @@
         cur-path-type
         (current-directory-list
          (directory-files-and-attributes directory t)))
-    ;; test
-    (message (concat "navigate: " directory))
+    (message (concat "navigate directory " directory))
     ;; while we are in the current directory
     (while current-directory-list
       (progn
@@ -136,7 +102,8 @@
         (setq cur-path-type (nth 1 (car current-directory-list)))
         (when (string-not-match-list cur-path except-list)
           ;; if type is file, append to tmp-files-list
-          (if (not cur-path-type)
+          ;;(if (not cur-path-type)
+          (if (is-file-attr(cdr (car current-directory-list)))
               ;; true
               (setq tmp-files-list
                     (cons cur-path tmp-files-list))
@@ -155,3 +122,48 @@
         (setq current-directory-list (cdr current-directory-list))))
     ;; return
     tmp-files-list))
+
+(defun index-files-and-save (target-file-name target-dir except-list)
+  "Index all files of HOME directory except containing user defined symbols."
+  (interactive)
+  (let ((tmp-list (files-under-dir-with-except
+                   target-dir
+                   except-list)))
+    (with-temp-buffer
+      (mapcar (lambda (f)
+                (insert f)
+                (insert "\n"))
+              tmp-list)
+      (when (file-writable-p target-file-name)
+        (message (concat "writing " target-file-name))
+        (write-region (point-min)
+                      (point-max)
+                      target-file-name)))))
+
+;; (defun index-home-files ()
+;;   "Index all files without some directory and file."
+;;   (interactive)
+;;   (index-files-and-save "~/.indexed_files.txt"
+;;                         "~"
+;;                         '("\\.Trash.*"
+;;                           "\\.macports.*"
+;;                           "\\.git.*"
+;;                           "\\.DS_Store"
+;;                           "\\/Backup"
+;;                           "\\/Private"
+;;                           "\\/Dropbox\\/devel"
+;;                           "\\/Dropbox\\/webdevel"
+;;                           "\\/Library"
+;;                           "\\/Downloads\\/targets"
+;;                           "\\.wmv$"
+;;                           "\\.avi$"
+;;                           "\\.mpg$"
+;;                           "\\.mp4$"
+;;                           "\\.mp3$"
+;;                           "\\.jpg$"
+;;                           "\\.jpeg$"
+;;                           "\\.png$"
+;;                           "\\.gif$"
+;;                           "\\.rar$"
+;;                           "\\.zip$"
+;;                           "\\.7z$")))
