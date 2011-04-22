3747acadf73b8b42d740556b1ef3b0de0a217026
Seorenn <hirenn@gmail.com>
2011-04-22 21:20:40 +0900

    changed anything more simple...

--- dotemacs.el
+++ dotemacs.el
@@ -44,7 +44,7 @@
 (load-my-library "renn-vim")
 (load-my-library "renn-org")
 (load-my-library "renn-markdown")
-;;(load-my-library "renn-anything")
+(load-my-library "renn-anything")
 (load-my-library "renn-twitter")
 (load-my-library "renn-erc")
 (load-my-library "renn-w3m")
--- renn-anything.el
+++ renn-anything.el
@@ -1,10 +1,19 @@
-(require 'anything-startup)
+;; (require 'anything-startup)
 
 ;; index file for anything-filelist+
 ;;(setq anything-c-filelist-file-name "~/.myfiles-index")
 
-(global-set-key "\C-x\C-f" 'anything-find-file)
-(global-set-key "\C-x\C-a\C-f" 'anything-for-files)
-(global-set-key [(ctrl tab)] 'anything-for-buffers)
+;; (global-set-key "\C-x\C-f" 'anything-find-file)
+;; (global-set-key "\C-x\C-a\C-f" 'anything-for-files)
+;; (global-set-key [(ctrl tab)] 'anything-for-buffers)
 
+(require 'anything)
+(require 'anything-config)
 
+(setq anything-sources
+      '(anything-c-source-buffers
+        anything-c-source-buffer-not-found
+        anything-c-source-file-name-history
+        anything-c-source-file-cache))
+
+(global-set-key (kbd "M-+") 'anything)
