007194c27f3f95b879aaf31dbddfdc65a8a2bb2e
Seorenn <hirenn@gmail.com>
2011-04-23 20:48:06 +0900

    added options for multi-term

--- renn-shell.el
+++ renn-shell.el
@@ -1,2 +1,12 @@
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; multi-term
+(autoload 'multi-term "multi-term" nil t)
+(autoload 'multi-term-next "multi-term" nil t)
+
+(setq multi-term-program "/bin/bash")
+
+;; [C-c t] command open new term, and switch next if one more terms running
+(global-set-key (kbd "C-c t") 'multi-term-next)
+(global-set-key (kbd "C-c T") 'multi-term)
