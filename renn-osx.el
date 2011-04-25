7c073aa5807654ff732481cb0e5bed03e46ed161
seorenn <hirenn@gmail.com>
2011-04-25 11:51:33 +0900

    updated font-set

--- renn-osx.el
+++ renn-osx.el
@@ -2,28 +2,70 @@
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
-;; Aquamacs options
 (setq inhibit-startup-echo-area-message t)
-;;(aquamacs-autoface-mode nil)
-;;(tabbar-mode nil)
-;;(one-buffer-one-frame-mode 0)
 
-;(set-face-font 'default "Monaco-11")
-(set-face-font 'default "-apple-Monaco-medium-normal-normal-*-12-120-72-72-m-120-iso10646-1")
+;; Aquamacs specific options ---------
+;; (aquamacs-autoface-mode nil)
+;; (tabbar-mode nil)
+;; (one-buffer-one-frame-mode 0)
 
-;; Korean
+;; Font Set -----------
+;; Hint) 1. Select font by Menu [Options - Set Default Font] - (select you wanted)
+;;       2. Move point to some character, then press C-u C-x =
+;;          (or command M-x what-cursor-position)
+;;       3. Emasc will display font-spec in splited window (buffer named *Help*)
+;;
+;; Tools) M-x anything-select-xfont will listing fonts list, but not all. :(
+
+;; 의미없는 설정인 듯...
+;; (add-to-list 'default-frame-alist '(font . "fontset-default"))
+
+(set-face-font 'default "Monaco-12")
+
+;; Korean Unicode SPEC.
+;; EUC-KR(cp949)은 조잡한 미완성 한글 스펙입니다.
+;; 쓰지맙시다. 윈도우를 죽입시다. 나쁜 윈도우!
+
+;; Mac OS X 기본 글꼴인 애플고딕 설정 예. 꼭 X-Font 스타일로 적을 필요는 없음.
+;; (set-fontset-font "fontset-default" '(#x1100 . #xffdc)
+;;                   "-apple-AppleGothic-medium-normal-normal-*-14-120-72-72-p-120-*-*")
+
+;; 나눔고딕(네이버 나눔글꼴 패키지)을 기본 한글 서체로 사용.
 (set-fontset-font "fontset-default" '(#x1100 . #xffdc)
-                  "-apple-AppleGothic-medium-normal-normal-*-14-120-72-72-p-120-*-*")
+                  '("NanumGothicOTF" . "iso10646-1"))
 
 ;; Unicode User Area
+;; (set-fontset-font "fontset-default" '(#xe0bc . #xf66e)
+;;                   "-apple-Hiragino_Kaku_Gothic_Pro-medium-normal-normal-*-14-120-72-72-p-120-iso10646-1")
+
 (set-fontset-font "fontset-default" '(#xe0bc . #xf66e)
-                  "-apple-Hiragino_Kaku_Gothic_Pro-medium-normal-normal-*-14-120-72-72-p-120-iso10646-1")
-;                  "-apple-AppleGothic-medium-normal-normal-*-14-120-72-72-p-120-*-*")
+                  '("NanumGothicOTF" . "iso10646-1"))
 
 ;; Japanese & Chinese
+;; I don't know Chinese. So configured all type of Chinese/Kanji/Han to Japanese Font Spec
+;; 일본어나 특수 한자를 쓰지 않는다면 이 설정은 의미 없을지도 모른답니다. ;)
+
 ;; (set-fontset-font "fontset-default" 'kana "Hiragino Kaku Gothic Pro-14")
 ;; (set-fontset-font "fontset-default" 'han "Hiragino Kaku Gothic Pro-14")
+;; (set-fontset-font "fontset-default" 'kana
+;;                   "-apple-Hiragino_Kaku_Gothic_Pro-medium-normal-normal-*-14-120-72-72-p-120-iso10646-1")
+;; (set-fontset-font "fontset-default" 'han
+;;                   "-apple-Hiragino_Kaku_Gothic_Pro-medium-normal-normal-*-14-120-72-72-p-120-iso10646-1-*")
+
 (set-fontset-font "fontset-default" 'kana
-                  "-apple-Hiragino_Kaku_Gothic_Pro-medium-normal-normal-*-14-120-72-72-p-120-iso10646-1")
+                  '("Hiragino Kaku Gothic Pro" . "iso10646-1"))
 (set-fontset-font "fontset-default" 'han
-                  "-apple-Hiragino_Kaku_Gothic_Pro-medium-normal-normal-*-14-120-72-72-p-120-iso10646-1-*")
+                  '("Hiragino Kaku Gothic Pro" . "iso10646-1"))
+(set-fontset-font "fontset-default" 'japanese-jisx0208
+                  '("Hiragino Kaku Gothic Pro" . "iso10646-1"))
+(set-fontset-font "fontset-default" 'katakana-jisx0201
+                  '("Hiragino Kaku Gothic Pro" . "iso10646-1"))
+
+(setq fixed-width-use-QuickDraw-for-ascii t) ; is need this? ;)
+(setq mac-allow-anti-aliasing t)             ; OK! I hate bitmap font
+
+;; Options for scales each font
+;; 각 폰트 사이의 크기를 기준폰트(default-font) 배수로 설정
+(setq face-font-rescale-alist
+      '((".*hiragino.*" . 1.2)
+        (".*nanum.*" . 1.3)))
