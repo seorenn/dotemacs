e4ac695f12cba08d10a557a4f0528136a19b80b0
seorenn <hirenn@gmail.com>
2011-04-26 12:11:04 +0900

    disabled vc-git

--- renn-magit.el
+++ renn-magit.el
@@ -1,4 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/magit")
 (require 'magit)
+;; uncomment if needed these:
 ;; (require 'magit-svn)
 ;; (require 'magit-topgit)
+
+;; disable vc-git
+(eval-after-load "vc" '(remove-hook 'find-file-hook 'vc-find-file-hook))
