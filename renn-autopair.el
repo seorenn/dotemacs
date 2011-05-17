dd0c0b710646f31ca7693fcaa534c27122a9b264
seorenn <hirenn@gmail.com>
2011-05-17 17:12:30 +0900

    added some configurations for python programming

--- renn-autopair.el
+++ renn-autopair.el
@@ -1,4 +1,13 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
 (require 'autopair)
-(autopair-global-mode)
+(autopair-global-mode 1)
+
+(setq autopair-autowrap t)
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (setq autopair-handle-action-fns
+                   (list 'autopair-default-handle-action
+                         'autopair-python-triple-quote-action))))
+
+(provide 'renn-autopair)
