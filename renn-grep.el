5ec245273e2e1c426d5001683dc1dde366d7dcca
seorenn <hirenn@gmail.com>
2011-05-27 19:06:36 +0900

    added grep-helper

---
+++ renn-grep.el
@@ -0,0 +1,20 @@
+(require 'grep)
+(setq grep-command-before-query "grep -nH -r -e ")
+(defun grep-default-command ()
+  (if current-prefix-arg
+      (let ((grep-command-before-target
+             (concat grep-command-before-query
+                     (shell-quote-argument (grep-tag-default)))))
+        (cons (if buffer-file-name
+                  (concat grep-command-before-target
+                          " *."
+                          (file-name-extension buffer-file-name))
+                (concat grep-command-before-target " ."))
+              (+ (length grep-command-before-target) 1)))
+    (car grep-command)))
+(setq grep-command (cons (concat grep-command-before-query " .")
+                         (+ (length grep-command-before-query) 1)))
+
+(define-key global-map (kbd "C-x g") 'grep)
+
+(provide 'renn-grep)
