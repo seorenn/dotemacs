cf6f2c16a2ea4597f9a4f2ef71bbd3483c378ba0
seorenn <hirenn@gmail.com>
2011-06-27 10:48:44 +0900

    turn on autopair-mode when specific mode activates

--- renn-autopair.el
+++ renn-autopair.el
@@ -1,7 +1,16 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
 (require 'autopair)
-(autopair-global-mode 1)
+;;(autopair-global-mode 1)
+(defun turn-on-autopair () (autopair-mode 1))
+(add-hook 'lisp-mode-hook 'turn-on-autopair)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-autopair)
+(add-hook 'c-mode-hook 'turn-on-autopair)
+(add-hook 'c++-mode-hook 'turn-on-autopair)
+(add-hook 'python-mode-hook 'turn-on-autopair)
+(add-hook 'js-mode-hook 'turn-on-autopair)
+(add-hook 'js2-mode-hook 'turn-on-autopair)
+(add-hook 'java-mode-hook 'turn-on-autopair)
 
 (setq autopair-autowrap t)
 (add-hook 'python-mode-hook
---
+++ renn-js2mode.el
@@ -0,0 +1,4 @@
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(provide 'renn-js2mode)
