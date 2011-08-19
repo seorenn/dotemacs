3fbc5ccd7e72173eafa90869c1640ca62ce11411
seorenn <hirenn@gmail.com>
2011-08-19 10:31:43 +0900

    disable multi-term-mode

--- renn-shell.el
+++ renn-shell.el
@@ -20,14 +20,15 @@
 (setenv "PAGER" "cat")
 
 ;; multi-term
-(autoload 'multi-term "multi-term" nil t)
-(autoload 'multi-term-next "multi-term" nil t)
+;;(autoload 'multi-term "multi-term" nil t)
+;;(autoload 'multi-term-next "multi-term" nil t)
 
-(setq multi-term-program "/bin/bash")
+;;(setq multi-term-program "/bin/bash")
 
 ;; [C-x t] command open new term or switch next if one or more terms running
-(global-set-key (kbd "C-x t") 'multi-term-next)
-(global-set-key (kbd "C-x T") 'multi-term)
+;;(global-set-key (kbd "C-x t") 'multi-term-next)
+(global-set-key (kbd "C-x t") 'term)
+;;(global-set-key (kbd "C-x T") 'multi-term)
 
 (add-hook 'term-mode-hook
           #'(lambda () (setq autopair-dont-activate t)))
