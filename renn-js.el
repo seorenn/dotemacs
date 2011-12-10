867d5aecd44ed1d7acfcba244b5fdc5bafe5e649
Seorenn <hirenn@gmail.com>
2011-12-10 23:11:46 +0900

    enabled jade-mode

--- renn-js.el
+++ renn-js.el
@@ -1,25 +1,4 @@
-;; ;; Configurations for JavaScript Editing...
-
-;; (autoload 'js2-mode "js2-mode" nil t)
-;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
-;; (setq js2-enter-indents-newline t)
-;; (setq js2-basic-offset 2)
-;; (setq js2-idle-timer-delay 5)
-
-;; ;; jade-mode
-;; (require 'sws-mode)
-;; (require 'jade-mode)
-;; (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
-;; (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
-
-;; ;; turn-off autopair mode when js2-mode activated
-;; ;;(add-hook 'js2-mode-hook
-;; ;;          #'(lambda () (setq autopair-dont-activate t)))
-
-;; (provide 'renn-js2mode)
-
-;; -------------------
+;; Configurations for JavaScript Editing...
 
 ;; Configurations for js3-mode
 (add-to-list 'load-path "~/.emacs.d/vendor/js3-mode")
@@ -37,5 +16,11 @@
 (setq js3-auto-indent-p t)
 (setq js3-enter-indents-newline t)
 (setq js3-indent-on-enter-key t)
+
+;; jade-mode
+(require 'sws-mode)
+(require 'jade-mode)
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
 
 (provide 'renn-js)
