fee56e1aaa9c976eb3e55bfc11c504d26dbc6d32
Seorenn <hirenn@gmail.com>
2012-03-31 19:33:32 +0900

    for js editing

--- renn-yasnippet.el
+++ renn-yasnippet.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
 
 (require 'yasnippet)
-(setq yas/snippet-dirs '("~/.emacs.d/vendor/yasnippet/snippets"))
+(setq yas/snippet-dirs '("~/.emacs.d/vendor/yasnippet/snippets" "~/.emacs.d/snippets"))
 (yas/global-mode 1)
 
 ;;(yas/initialize)
