577490dcffb0dc92cccf8379ae1ff6c2d9e46e50
Heeseung Seo <hirenn@websync.co.kr>
2012-05-29 19:59:16 +0900

    added calfw and examples

--- dotemacs.el
+++ dotemacs.el
@@ -58,5 +58,6 @@
 (load-my-library "renn-colortheme")
 (load-my-library "renn-shortcuts")
 (load-my-library "renn-speedbar")
+(load-my-library "renn-calfw")
 
 (cd "~/")
---
+++ renn-calfw.el
@@ -0,0 +1,4 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/emacs-calfw")
+(require 'calfw)
+(require 'calfw-org)
+(require 'calfw-ical)
