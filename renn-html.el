d249a28966f7de30f677caa66728576ebcd564f2
Seorenn <hirenn@gmail.com>
2012-09-20 19:44:32 +0900

    Added support scss-mode

--- renn-html.el
+++ renn-html.el
@@ -14,3 +14,14 @@
                   (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
 (setq mweb-filename-extensions '("php" "html" "html" "ctp" "phtml" "php4" "php5"))
 (multi-web-global-mode 1)
+
+;;;; scss-mode
+;; https://github.com/antonj/scss-mode
+
+(add-to-list 'load-path "~/.emacs.d/vendor/scss-mode")
+(autoload 'scss-mode "scss-mode")
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(defun scss-custom ()
+  "custom scss-mode-hook"
+  (flymake-mode))
+(add-hook 'scss-mode 'scss-custom)
