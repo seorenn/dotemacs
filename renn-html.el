3fb6036297ba92a78b1f1cd86bc9d74038def5fb
Seorenn <hirenn@gmail.com>
2012-09-20 12:38:39 +0900

    Perform multi-web-mode
    - Removed nxhtml-mode
    - Added multi-web-mode
    - Perform multi-web-mode with js2-mode

--- renn-html.el
+++ renn-html.el
@@ -3,20 +3,14 @@
             (set (make-local-variable 'sgml-basic-offset) 2)
             (sgml-guess-indent)))
 
-;; Mumamo is making emacs 23.3 freak out:
-(when (and (equal emacs-major-version 23)
-           (>= emacs-minor-version 3))
-  (eval-after-load "bytecomp"
-    '(add-to-list 'byte-compile-not-obsolete-vars
-                  'font-lock-beginning-of-syntax-function))
-  ;; tramp-compat.el clobbers this variable!
-  (eval-after-load "tramp-compat"
-    '(add-to-list 'byte-compile-not-obsolete-vars
-                  'font-lock-beginning-of-syntax-function)))
+;;;; multi-web-mode
+;; https://github.com/fgallina/multi-web-mode
 
-;; nxhtml-mode
-
-(add-to-list 'load-path "~/.emacs.d/vendor/nxhtml")
-(load "~/.emacs.d/vendor/nxhtml/autostart.el")
-; no background color for mumamo
-(setq mumamo-background-colors nil)
+(add-to-list 'load-path "~/.emacs.d/vendor/multi-web-mode")
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (js2-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "html" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
