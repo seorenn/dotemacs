ee8d7b341095aca5c83315081fca0cdd6005b5a6
Heeseung Seo <hirenn@websync.co.kr>
2013-02-19 12:38:50 +0900

    added yasnippet to git submodule

--- renn-yasnippet.el
+++ renn-yasnippet.el
@@ -1,11 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
 
 (require 'yasnippet)
-(setq yas/snippet-dirs '("~/.emacs.d/vendor/yasnippet/snippets" "~/.emacs.d/snippets"))
-(yas/global-mode 1)
+(setq yas-snippet-dirs '("~/.emacs.d/vendor/yasnippet/snippets" "~/.emacs.d/snippets"))
+(yas-global-mode 1)
 
-;;(yas/initialize)
-;;(yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
-
-; for nxhtml-mode
-(yas/define-snippets 'nxhtml-mode nil 'html-mode)
+;;;; for nxhtml-mode
+;(yas-define-snippets 'nxhtml-mode nil 'html-mode)
