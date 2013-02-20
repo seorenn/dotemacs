4765fbe2b4e69a4dd6543b16acc4cbcd53bd14f2
Heeseung Seo <hirenn@websync.co.kr>
2013-02-20 13:29:58 +0900

    Update

--- update_submodules.sh
+++ update_submodules.sh
@@ -4,19 +4,7 @@ git submodule sync
 git submodule foreach git pull origin master
 git submodule foreach git checkout master
 
-cd emacs-git-gutter
-make
-cd -
-
-cd js2-mode
-make
-cd -
-
-cd magit
-make
-cd -
-
-cd pymacs
+cd vendor/pymacs
 make
 make install
 cd -
