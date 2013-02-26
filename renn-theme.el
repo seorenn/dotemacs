85a7a64bcd990d7e63694213f6ec1d2f5f670371
Heeseung Seo <hirenn@websync.co.kr>
2013-02-26 17:52:56 +0900

    removes unused extentions

--- renn-theme.el
+++ renn-theme.el
@@ -1,5 +1,3 @@
-;(require 'facemenu+)
-
 ;; with Emacs 24
 ;(load-theme 'cyberpunk t)
 ;(color-theme-sanityinc-tomorrow-bright)
@@ -11,36 +9,3 @@
 
 ;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 ;(add-to-list 'load-path "~/.emacs.d/themes")
-
-
-;;(load "term/xterm")
-
-;; (defun my-solarized-term-theme ()
-;;   (interactive)
-;;   ;(load "term/xterm")
-;;   (xterm-register-default-colors)
-;;   (tty-set-up-initial-frame-faces)
-;;   (setq solarized-termcolors 16)
-;;   (setq solarized-use-terminal-theme t)
-;;   (load-theme 'solarized-dark t))
-
-;;(if window-system
-;;    (require 'tomorrow-night-bright-theme))
-;(require 'tomorrow-night-bright-theme)
-
-;; (if window-system
-;;     (require 'solarized-light-theme)
-;;   (require 'solarized-dark-theme))
-
-;(load-theme 'solarized-dark t)
-;(require 'solarized-definitions)
-;(if window-system
-;    (require 'solarized-dark-theme)
-;  (load-theme 'solarized-dark t))
-;;   ;(load-theme 'solarized-dark t))
-;;   (my-solarized-term-theme))
-
-;(load-theme solarized-dark t)
-
-;(require 'face-list)
-;; M-x list-faces-display
