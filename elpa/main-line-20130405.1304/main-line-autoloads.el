9895ce70ed6284f07cb9a46dc468615f0cbdab3a
Heeseung Seo <hirenn@websync.co.kr>
2013-09-04 15:20:01 +0900

    Add main-line extensions and settings

---
+++ elpa/main-line-20130405.1304/main-line-autoloads.el
@@ -0,0 +1,18 @@
+;;; main-line-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("main-line-pkg.el" "main-line.el") (21030
+;;;;;;  51771 21149 0))
+
+;;;***
+
+(provide 'main-line-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; main-line-autoloads.el ends here
--- renn-modeline.el
+++ renn-modeline.el
@@ -1,50 +1,56 @@
-(defun shorten-directory (dir max-length)
-  "Show up to `max-length' characters of a directory name `dir'."
-  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
-        (output ""))
-    (when (and path (equal "" (car path)))
-      (setq path (cdr path)))
-    (while (and path (< (length output) (- max-length 4)))
-      (setq output (concat (car path) "/" output))
-      (setq path (cdr path)))
-    (when path
-      (setq output (concat ".../" output)))
-    output))
+(set-face-attribute 'mode-line nil
+                    :box nil)
 
-(setq-default mode-line-format
-      (list
-       " "
-       '(:eval (propertize "%b" 'face 'font-lock-type-face
-                           'help-echo (buffer-file-name)))
+(require 'main-line)
+(setq main-line-separator-style 'wave)
 
-       " ("
-       (propertize "%02i" 'face 'font-lock-type-face) ","
-       (propertize "%02c" 'face 'font-lock-type-face)
-       ")"
+;; (defun shorten-directory (dir max-length)
+;;   "Show up to `max-length' characters of a directory name `dir'."
+;;   (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
+;;         (output ""))
+;;     (when (and path (equal "" (car path)))
+;;       (setq path (cdr path)))
+;;     (while (and path (< (length output) (- max-length 4)))
+;;       (setq output (concat (car path) "/" output))
+;;       (setq path (cdr path)))
+;;     (when path
+;;       (setq output (concat ".../" output)))
+;;     output))
 
-       "["
-       (propertize "%p" 'face 'font-lock-constant-face)
-       "/"
-       (propertize "%I" 'face 'font-lock-constant-face)
-       "]"
+;; (setq-default mode-line-format
+;;       (list
+;;        " "
+;;        '(:eval (propertize "%b" 'face 'font-lock-type-face
+;;                            'help-echo (buffer-file-name)))
 
-       "["
-       '(:eval (propertize "%m" 'face 'font-lock-comment-face
-                           'help-echo buffer-file-coding-system))
-       "]"
+;;        " ("
+;;        (propertize "%02i" 'face 'font-lock-type-face) ","
+;;        (propertize "%02c" 'face 'font-lock-type-face)
+;;        ")"
 
-       "["
-       '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-                           'face 'font-lock-reference-face
-                           'help-echo (concat "Buffer is in "
-                                              (if overwrite-mode "overwrite" "insert") " mode")))
-       '(:eval (when (buffer-modified-p)
-                 (concat "," (propertize "Mod"
-                                         'face 'font-lock-regexp-grouping-construct
-                                         'help-echo "Buffer has been modified"))))
-       '(:eval (when buffer-read-only
-                 (concat "," (propertize "RO"
-                                         'face 'font-lock-type-face
-                                         'help-echo "Buffer is read-only"))))
-       "] "
-       "%-"))
+;;        "["
+;;        (propertize "%p" 'face 'font-lock-constant-face)
+;;        "/"
+;;        (propertize "%I" 'face 'font-lock-constant-face)
+;;        "]"
+
+;;        "["
+;;        '(:eval (propertize "%m" 'face 'font-lock-comment-face
+;;                            'help-echo buffer-file-coding-system))
+;;        "]"
+
+;;        "["
+;;        '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+;;                            'face 'font-lock-reference-face
+;;                            'help-echo (concat "Buffer is in "
+;;                                               (if overwrite-mode "overwrite" "insert") " mode")))
+;;        '(:eval (when (buffer-modified-p)
+;;                  (concat "," (propertize "Mod"
+;;                                          'face 'font-lock-regexp-grouping-construct
+;;                                          'help-echo "Buffer has been modified"))))
+;;        '(:eval (when buffer-read-only
+;;                  (concat "," (propertize "RO"
+;;                                          'face 'font-lock-type-face
+;;                                          'help-echo "Buffer is read-only"))))
+;;        "] "
+;;        "%-"))
