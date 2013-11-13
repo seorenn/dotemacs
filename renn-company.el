c8e2dacc422e693e041378ad23fea7570a61dafe
seorenn <hirenn@iplateia.com>
2013-11-13 10:39:48 +0900

    Why company-mode not works automatically??? -_-

--- renn-company.el
+++ renn-company.el
@@ -1,9 +1,13 @@
+;; Configurations for company-mode
+
+(require 'company)
+
 (add-hook 'after-init-hook 'global-company-mode)
 
-(defun indent-or-complete ()
-  (interactive)
-  (if (looking-at "\\_>")
-      (company-complete-common)
-    (indent-according-to-mode)))
+;; (defun indent-or-complete ()
+;;   (interactive)
+;;   (if (looking-at "\\_>")
+;;       (company-complete-common)
+;;     (indent-according-to-mode)))
 
-(global-set-key "\t" 'indent-or-complete)
+;; (global-set-key "\t" 'indent-or-complete)
