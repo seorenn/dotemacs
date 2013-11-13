f4ceb022dd0b76db045020efc2bfec7c9e830ec0
Heeseung Seo <hirenn@RennMPB.local>
2013-11-13 10:45:58 +0900

    Company-mode will works with Yasnippets! ;-)

--- renn-company.el
+++ renn-company.el
@@ -4,10 +4,26 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
-;; (defun indent-or-complete ()
-;;   (interactive)
-;;   (if (looking-at "\\_>")
-;;       (company-complete-common)
-;;     (indent-according-to-mode)))
+(defun check-expansion ()
+  (save excursion
+        (if (looking-at "\\_>") t
+          (backward-char 1)
+          (if (looking-at "\\.") t
+            (backwrd-char 1)
+            (if (looking-at "->") t nil)))))
 
-;; (global-set-key "\t" 'indent-or-complete)
+(defun do-yas-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (yas/expand)))
+
+(defun tab-indent-or-complete ()
+  (interactive)
+  (if (minibufferp)
+      (minibuffer-complete)
+    (if (or (not yas/minor-mode)
+            (null (do-yas-expand)))
+        (if (check-expansion)
+            (company-complete-common)
+          (indent-for-tab-command)))))
+
+(global-set-key [tab] 'tab-indent-or-complete)
