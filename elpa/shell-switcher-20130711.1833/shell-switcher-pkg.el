0ad22390fe353ec8b69b09fffd6339c6f5cf108e
Heeseung Seo <hirenn@RennMPB.local>
2013-11-21 13:59:51 +0900

    shell-switcher, resize default frame size for my small macbook ;-)

---
+++ elpa/shell-switcher-20130711.1833/shell-switcher-pkg.el
@@ -0,0 +1,1 @@
+(define-package "shell-switcher" "20130711.1833" "Provide fast switching between shell buffers" 'nil)
--- renn-gui.el
+++ renn-gui.el
@@ -1,4 +1,4 @@
 (tool-bar-mode -1)
 
-(add-to-list 'default-frame-alist '(width . 200))
-(add-to-list 'default-frame-alist '(height . 60))
+(add-to-list 'default-frame-alist '(width . 140))
+(add-to-list 'default-frame-alist '(height . 47))
