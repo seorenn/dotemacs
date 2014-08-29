b6c246a160f55da834b03c0c7594baac8dc56c08
seorenn <hirenn@iplateia.com>
2014-08-29 15:09:57 +0900

    Disable MELPA, Marmalade. Just activate MELPA-Stable Only

--- lisp/renn-packages.el
+++ lisp/renn-packages.el
@@ -1,6 +1,7 @@
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (setq package-list
  '(ac-nrepl
